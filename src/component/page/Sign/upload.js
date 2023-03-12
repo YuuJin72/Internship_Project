f46662c26940c55f2b275f904d2ad13dd57ac3eb
YuuJin72 <kkjo9573@gmail.com>
2023-03-12 23:50:06 +0900

    #13 [Feature] 스터디룸 게시물 수정/삭제 기능

--- src/component/page/Sign/upload.js
+++ src/component/page/Sign/upload.js
@@ -3,36 +3,36 @@ import FileUpload from "react-mui-fileuploader";
 import { Button } from "@mui/material";
 import axios from "axios";
 
-function MuiFileUploader() {
-  const [filesToUpload, setFilesToUpload] = useState([]);
+// function MuiFileUploader() {
+//   const [filesToUpload, setFilesToUpload] = useState([]);
 
-  const handleFilesChange = (files) => {
-    // Update chosen files
-    setFilesToUpload([...files]);
-  };
+//   const handleFilesChange = (files) => {
+//     // Update chosen files
+//     setFilesToUpload([...files]);
+//   };
 
-  const uploadFiles = () => {
-    // Create a form and post it to server
-    let formData = new FormData();
-    filesToUpload.forEach((file) => formData.append("files", file));
+//   const uploadFiles = () => {
+//     // Create a form and post it to server
+//     let formData = new FormData();
+//     filesToUpload.forEach((file) => formData.append("files", file));
 
-    axios.post("http://localhost:8080/upload", {
-      body: formData
-    });
-  };
+//     axios.post("http://localhost:8080/upload", {
+//       body: formData
+//     });
+//   };
 
-  return (
-    <>
-      <FileUpload
-        multiFile={true}
-        onFilesChange={handleFilesChange}
-        onContextReady={(context) => {}}
-      />
-      <Button onClick={uploadFiles} variant="contained" id="uploadButton">
-        Upload
-      </Button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <FileUpload
+//         multiFile={true}
+//         onFilesChange={handleFilesChange}
+//         onContextReady={(context) => {}}
+//       />
+//       <Button onClick={uploadFiles} variant="contained" id="uploadButton">
+//         Upload
+//       </Button>
+//     </>
+//   );
+// }
 
-export default MuiFileUploader
+// export default MuiFileUploader
